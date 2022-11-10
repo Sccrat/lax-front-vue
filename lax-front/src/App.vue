2b566597395ffe1bafc3495b07f2e227d086605f
Romario <roma.713@hotmail.com>
2022-11-10 02:11:48 -0500

    todo

---
+++ lax-front/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Table from './views/Table.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Table />
+    </div>
+  </header>
+</template>
+
---
+++ lax-front/src/views/Table.vue
@@ -0,0 +1,49 @@
+
+<template>
+    <div>
+        <table class="table ">
+            <thead>
+                <tr>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Positivos</th>
+                    <th scope="col">Negativos</th>
+                    <th scope="col">Pendientes</th>
+                    <th scope="col">Muertes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in covid">
+                    <td>{{ item.Date }}</td>
+                    <td>{{ item.Muertes }}</td>
+                    <td>{{ item.Negativos }}</td>
+                    <td>{{ item.Pendientes }}</td>
+                    <td>{{ item.Positivos }}</td>
+                </tr>
+               
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script lang="ts">
+    import  axios  from "axios";
+    export default {
+        data(){
+            return{
+                covid:null
+            }
+        },mounted(){
+            this.getCovid();
+        },methods:{
+            getCovid(){
+                axios.get('localhost:3001/coviddaily').then(response =>{
+                    console.log(response.data);
+                    this.covid = response.data
+                }).catch(e=>console.log(e)
+                )
+
+            }
+        }
+    }
+</script>
+
